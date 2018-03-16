b6684ede4ab278f04cb76e4ef1c8bf3bab629851
Johannes Hofmann <johannes_hofmann@me.com>
2018-03-16 13:12:48 +0100

    adds examples to documentation

--- docs/src/components/App.vue
+++ docs/src/components/App.vue
@@ -1,5 +1,33 @@
 <template>
-  <VueBucketLoader/>
+  <div>
+    <h1>VueBucketLoader</h1>
+    <p>A simple vue component to upload files to s3</p>
+
+    <h2>Basic Implementation</h2>
+    <p>When your presigning endpoint is the same for any upload:</p>
+    <code>
+      {{ example }}
+    </code>
+    <VueBucketLoader
+      :signingUrl="'https://httpbin.org/post'"
+    />
+
+    <h2>Url generation using callback function</h2>
+    <p>When your presinging endpoint must be generated dynamically:</p>
+    <code>...</code>
+    <VueBucketLoader
+      :signingUrl="'https://httpbin.org/post'"
+    />
+
+    <h2>Checking Mime Types</h2>
+    <p>...</p>
+    <code>...</code>
+    <VueBucketLoader
+      :signingUrl="'https://httpbin.org/post'"
+      :beforeUpload="checkMimeType"
+    />
+
+  </div>
 </template>
 
 <script>
@@ -8,6 +36,17 @@ import VueBucketLoader from '../../../src/components/VueBucketLoader';
 export default {
   components: {
     VueBucketLoader,
+  },
+
+  data: () => ({
+    example: `
+      <VueBucketLoader
+        :presignedUrlEndpoint="'https://httpbin.org/post'"
+      />`,
+  }),
+
+  methods: {
+    checkMimeType: file => ['image/png', 'image/jpg'].includes(file.type),
   },
 };
 </script>
